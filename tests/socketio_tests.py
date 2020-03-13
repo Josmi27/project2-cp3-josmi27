89108e03c83806f606866c568582f4f1c12247a6
Joshua Smith <30632852+Josmi27@users.noreply.github.com>
2020-03-13 15:15:14 -0400

    Successfully completed and ran all socketio tests

--- tests/socketio_tests.py
+++ tests/socketio_tests.py
@@ -2,49 +2,92 @@ import chatchat
 import unittest
 import ap
 
-# These should test Socket.io codepaths ([responses for client requests], so when
-# the client sends a request, assertEqual the server response) within 
-# your app and be unique to each other
-
-# NEED 5
-
 class socketio_test(unittest.TestCase):
     
-    def test_server_sends_hello(self):
+    def test_server_client_connects(self):
         client = ap.socketio.test_client(ap.app)
-        response = client.get_received()
-        self.assertEquals(len(response), 1)
-        
-        from_server = response[0]
-        self.assertEqual(from_server['name'], "hello to client")
-        
-        data = from_server['args'][0]
-        self.assertEqual(data['message'], "Hey there!")
-        
-        
-        
-        
-    def test_server_relays_message(self):
+        self.assertTrue(client.is_connected())
+    
+    def test_server_client_disconnects(self):
+        global disconnected
+        disconnected = None
         client = ap.socketio.test_client(ap.app)
-        client.emit('new message', {'my message': 'Purple is beautiful!'})
-        response = client.get_received()
-        self.assertEqual(len(response), 2)
+        client.disconnect()
+        self.assertEqual(disconnected, None)
+    
+    def test_emit_namespace(self):
+        client = ap.socketio.test_client(ap.app)
+        receive = client.get_received()
+        print(receive)
+
+        client.emit('Entered data', {
+            'username': "Joshua",
+            'message': "My message"
+        })
+
+        received = client.get_received()
+        self.assertEqual(received[0]['namespace'][0], '/')   
+    
+    def test_emit_server_response(self):
+        client = ap.socketio.test_client(ap.app)
+        receive = client.get_received()
+        # print(receive)
+
+        client.emit('Entered data', {
+            'username': "Joshua",
+            'message': "My message"
+        })
+
+        received = client.get_received()
+        self.assertEqual(received[0]['name'], 'Info is now sending from server')
+    
+    
+    
+    def test_client_data_to_serverS(self):
+        client = ap.socketio.test_client(ap.app)
+        receive = client.get_received()
+
+        client.emit('Entered data', {
+            'username': "Joshua",
+            'message': "My message"
+        })
+
+        received = client.get_received()
+        self.assertEqual(received[0]['args'][0]['name'], 'Joshua')
+        self.assertEqual(received[0]['args'][0]['post'], 'My message')
         
-        from_server = response[1]
-        # print("I am from_Server variable: ", from_server)
-        self.assertEqual(from_server['name'], 'got your message')
+
+    
+    #CHATCHAT TESTS
+    
+    def test_socketio_case_chatbot_aboutMessage(self, usern="Joshua", message="!! about"): 
+        self.usern = usern
+        self.message = message
+        result = chatchat.ChatBot(usern, message)[1]
+        self.assertEqual(result, "Welcome to the chatroom of brotherhood. I was created to uphold the unification of Iotas nationally. And always remember our 5 principles: Scholarship, Leadership, Citizenship, Fidelity, and Brotherhood!")
         
-        data = from_server['args'][0]
-        self.assertEqual(data['your message'], u'Purple is beautiful!')
-        print(response[1])
+
+    def test_socketio_case_chatbot_helpMessage(self, usern="Joshua", message="!! help"): 
+        self.usern = usern
+        self.message = message
+        result = chatchat.ChatBot(usern, message)[1]
+        self.assertEqual(result, "The following commands are available to you: '!! about' | '!! help' | '!! say <something>' | '!! service date' | '!! party date' | '!! brotherhood quote'")
 
 
-
-
+    def test_socketio_case_chatbot_serviceMessage(self, usern="Joshua", message="!! service date"): 
+        self.usern = usern
+        self.message = message
+        result = chatchat.ChatBot(usern, message)[1]
+        self.assertEqual(result,  "At this current time, the next community service date will be held on April 23, following the regional convention.")
+    
+    def test_socketio_case_chatbot_partyMessage(self, usern="Joshua", message="!! party date"): 
+        self.usern = usern
+        self.message = message
+        result = chatchat.ChatBot(usern, message)[1]
+        self.assertEqual(result,  "You already know when the party is {}, stop playing with me brother!".format(usern)) 
 
 
 
 if __name__ == '__main__':
     unittest.main()
     # To run: python -m tests.socketio_tests in parent folder
-    
